d800090f875854c1cbda89dba0e868c8eb0cb32e
sachin <sachinishanga91@gmail.com>
2026-01-27 16:08:36 +0530

    init commit

--- src/main/java/com/example/layeredarchitecture/dao/OrderDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/OrderDAOImpl.java
@@ -1,7 +1,6 @@
 package com.example.layeredarchitecture.dao;
 
 import com.example.layeredarchitecture.dao.custom.OrderDAO;
-import com.example.layeredarchitecture.db.DBConnection;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -9,31 +8,16 @@ import java.time.LocalDate;
 public class OrderDAOImpl implements OrderDAO {
 
     public String generateOderId() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
+        ResultSet rst = CrudUtil.execute("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
         return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
     }
 
     public boolean existOrderID(String orderID) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
-        stm.setString(1, orderID);
-        ResultSet rst = stm.executeQuery();
+        ResultSet rst = CrudUtil.execute("SELECT oid FROM `Orders` WHERE oid=?");
         return rst.next();
     }
 
     public boolean saveOrder(String orderId,String customerId , LocalDate date) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
-        stm.setString(1, orderId);
-        stm.setDate(2, Date.valueOf(date));
-        stm.setString(3, customerId );
-        return stm.executeUpdate() > 0;
+        return CrudUtil.execute("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
     }
-
-
-
-
-
 }
--- src/main/java/com/example/layeredarchitecture/dao/OrderDetailDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/OrderDetailDAOImpl.java
@@ -1,21 +1,11 @@
 package com.example.layeredarchitecture.dao;
 
 import com.example.layeredarchitecture.dao.custom.OrderDetailDAO;
-import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.OrderDetailDTO;
-
 import java.sql.*;
 
 public class OrderDetailDAOImpl implements OrderDetailDAO {
-
      public boolean saveOrderDetails(String orderId,OrderDetailDTO orderDetail) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
-
-        stm.setString(1, orderId);
-        stm.setString(2, orderDetail.getItemCode());
-        stm.setBigDecimal(3, orderDetail.getUnitPrice());
-        stm.setInt(4, orderDetail.getQty());
-        return stm.executeUpdate() == 1;
+        return CrudUtil.execute("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
     }
 }
